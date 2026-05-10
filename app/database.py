90c3f558eede4562a3f4f94fb572b75cb4753dfb
s._.giii <su06179@gmail.com>
2026-05-10 19:06:04 +0900

    [Refactor]DataBase_URL 환경 변수 분리

--- app/database.py
+++ app/database.py
@@ -1,20 +1,22 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 
-# DB 주소
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:0617@localhost:5432/capstone_gis"
+load_dotenv()
 
-# DB 엔진 생성
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@localhost:5432/capstone_gis"
+)
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# DB 세션 생성기
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# ORM 모델의 기본 클래스
 Base = declarative_base()
 
-# DB 세션 의존성 주입 함수 (API 호출 시 사용)
 def get_db():
     db = SessionLocal()
     try:
